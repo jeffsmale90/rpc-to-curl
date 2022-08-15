84d89e40861a09d74468bbe51babb93e8365e103
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2022-08-16 10:35:58 +1200

    Add support for .env configuration

--- cli.js
+++ cli.js
@@ -2,17 +2,34 @@
 
 import { Command } from "commander";
 import { toCurl } from "./index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const hostsString = process.env.TOCURL_HOSTS;
+
+let hosts = {
+  localhost: "http://localhost:8545",
+};
+if (hostsString !== undefined) {
+  hosts = {
+    ...hosts,
+    ...JSON.parse(hostsString),
+  };
+}
 
 const command = new Command();
 
 command
   .option("-o|--output-only", "output CURL command only, don't execute.", false)
-  .option("-u|--url <string>", "RPC URL", "http://localhost:8545");
+  .option("-u|--url <string>", "RPC URL", hosts.localhost);
 command.parse();
 const options = command.opts();
 
 const isOutputOnly = options.outputOnly;
-const url = options.url;
+let url = options.url;
+if (hosts[url]) {
+  url = hosts[url];
+}
 const parameterString = command.args[0];
 
 toCurl({
